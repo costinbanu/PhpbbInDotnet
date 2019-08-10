f4083c593f19269bcbc3c04aff53aa5e355f40b1
Costin Banu <costin@skymail.ro>
2019-08-10 12:13:47 +0200

    almost working login

---
+++ Serverless.Forum/Serverless.Forum/Pages/Login.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Serverless.Forum.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public string username;
+        public string password;
+        public bool rememberMe;
+        public string returnUrl;
+
+        public void OnGet(string ReturnUrl)
+        {
+            returnUrl = ReturnUrl;
+        }
+
+        public void OnPost(string username, string password, string returnUrl, bool rememberMe)
+        {
+            var x = HttpContext.Request.Query["ReturnUrl"];
+        }
+    }
+}
