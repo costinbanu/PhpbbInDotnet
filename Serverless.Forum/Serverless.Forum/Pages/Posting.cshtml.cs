6176e510b53476d3392e99be1f37b63e4d0d48cf
Costin Banu <costin@skymail.ro>
2019-09-20 18:53:44 +0200

    WIP: posting now has working javascript editor

--- Serverless.Forum/Serverless.Forum/Pages/Posting.cshtml.cs
+++ Serverless.Forum/Serverless.Forum/Pages/Posting.cshtml.cs
@@ -7,10 +7,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Serverless.Forum.Contracts;
 using Serverless.Forum.forum;
 using Serverless.Forum.Utilities;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,42 +23,128 @@ namespace Serverless.Forum.Pages
 {
     public class PostingModel : PageModel
     {
-        forumContext _dbContext;
-        IAmazonS3 _s3Client;
-        IConfiguration _config;
-        bool hasAttachments = false;
+        private const string REPLY = "Re: ";
 
+        private readonly forumContext _dbContext;
+        private readonly IAmazonS3 _s3Client;
+        private readonly IConfiguration _config;
+        private bool hasAttachments = false;
+
+        public LoggedUser CurrentUser
+        {
+            get
+            {
+                var user = User;
+                if (!user.Identity.IsAuthenticated)
+                {
+                    user = Acl.Instance.GetAnonymousUser(_dbContext);
+                    Task.WaitAll(
+                        HttpContext.SignInAsync(
+                            CookieAuthenticationDefaults.AuthenticationScheme, 
+                            user, 
+                            new AuthenticationProperties
+                            {
+                                AllowRefresh = true,
+                                ExpiresUtc = DateTimeOffset.Now.AddMonths(1),
+                                IsPersistent = true,
+                            }
+                        )
+                    );
+                }
+                return user.ToLoggedUser();
+            }
+        }
+
+        //https://stackoverflow.com/questions/54963951/aws-lambda-file-upload-to-asp-net-core-2-1-razor-page-is-corrupting-binary
         [BindProperty]
-        //[Required]
-        //[Display(Name = "Workbook")]
+        [Required]
+        [Display(Name = "Attachment")]
         public IFormFile Attachment { get; set; }
+
+        public string TopicTitle { get; set; }
+        public string PostTitle { get; set; }
+        public string PostText { get; set; }
+        public int ForumId { get; set; }
+        public int TopicId { get; set; }
 
         public PostingModel(forumContext context, IConfiguration config)
         {
             _dbContext = context;
             _config = config;
             _s3Client = new AmazonS3Client(_config["AwsS3Key"], _config["AwsS3Secret"], RegionEndpoint.EUCentral1);
-
         }
 
-        public async Task OnPostAttachment()
+        public IActionResult OnGetForumPost(int forumId, int topicId)
         {
-            var user = User;
-            if (!user.Identity.IsAuthenticated)
+            ForumId = forumId;
+            TopicId = topicId;
+            var curTopic = (from t in _dbContext.PhpbbTopics
+                            where t.TopicId == topicId
+                            select t).FirstOrDefault();
+
+            if (curTopic == null)
             {
-                user = Acl.Instance.GetAnonymousUser(_dbContext);
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, user, new AuthenticationProperties
-                {
-                    AllowRefresh = true,
-                    ExpiresUtc = DateTimeOffset.Now.AddMonths(1),
-                    IsPersistent = true,
-                });
+                return NotFound();
+            }
+
+            TopicTitle = curTopic.TopicTitle;
+            PostTitle = $"{REPLY}{TopicTitle}";
+
+            return Page();
+        }
+
+        public IActionResult OnGetQuoteForumPost(int postId)
+        {
+            var curPost = (from p in _dbContext.PhpbbPosts
+                           where p.PostId == postId
+                           select p).FirstOrDefault();
+
+            if (curPost == null)
+            {
+                return NotFound();
+            }
+
+            ForumId = curPost.ForumId;
+            TopicId = curPost.TopicId;
+
+            var curTopic = (from t in _dbContext.PhpbbTopics
+                            where t.TopicId == TopicId
+                            select t).FirstOrDefault();
+
+            if (curTopic == null)
+            {
+                return NotFound();
+            }
+
+            TopicTitle = curTopic.TopicTitle;
+
+            var subject = curPost.PostSubject.StartsWith(REPLY) ? curPost.PostSubject.Substring(REPLY.Length) : curPost.PostSubject;
+            PostTitle = PostTitle = $"{REPLY}{subject}";
+
+            var curAuthor = curPost.PostUsername;
+            if (string.IsNullOrWhiteSpace(curAuthor))
+            {
+                curAuthor = (from u in _dbContext.PhpbbUsers
+                              where u.UserId == curPost.PosterId
+                              select u.Username).FirstOrDefault();
+            }
+
+            PostText = $"[quote=\"{curAuthor}\"]{Environment.NewLine}{HttpUtility.HtmlDecode(curPost.PostText)}{Environment.NewLine}[/quote]";
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAttachment()
+        {
+            if (CurrentUser.UserId == 1)
+            {
+                return Unauthorized();
             }
 
             var request = new PutObjectRequest
             {
                 BucketName = _config["AwsS3BucketName"],
-                Key = $"{user.ToLoggedUser().UserId?.ToString()}_{Guid.NewGuid():n}".ToLowerInvariant(),
+                Key = $"{CurrentUser.UserId ?? 0}_{Guid.NewGuid():n}".ToLowerInvariant(),
                 ContentType = Attachment.ContentType,
                 InputStream = Attachment.OpenReadStream()
             };
@@ -67,25 +156,13 @@ namespace Serverless.Forum.Pages
             }
 
             hasAttachments = true;
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostForumPost(int forumId, int topicId, string postSubject, string postText, string returnUrl)
         {
-            var user = User;
-            if (!user.Identity.IsAuthenticated)
-            {
-                user = Acl.Instance.GetAnonymousUser(_dbContext);
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, user, new AuthenticationProperties
-                {
-                    AllowRefresh = true,
-                    ExpiresUtc = DateTimeOffset.Now.AddMonths(1),
-                    IsPersistent = true,
-                });
-            }
-
-            var loggedUser = user.ToLoggedUser();
-
-            if (loggedUser.UserId == 1)
+            if (CurrentUser.UserId == 1)
             {
                 return Unauthorized();
             }
@@ -94,7 +171,7 @@ namespace Serverless.Forum.Pages
             {
                 ForumId = forumId,
                 TopicId = topicId,
-                PosterId = loggedUser.UserId.Value,
+                PosterId = CurrentUser.UserId.Value,
                 PostSubject = HttpUtility.HtmlEncode(postSubject),
                 PostText = HttpUtility.HtmlEncode(postText),
                 PostTime = DateTime.UtcNow.LocalTimeToTimestamp(),
@@ -122,7 +199,7 @@ namespace Serverless.Forum.Pages
 
         public async Task<IActionResult> OnPostPrivateMessage()
         {
-
+            throw await Task.FromResult(new NotImplementedException());
         }
 
         private string RandomString(int length = 8)
@@ -141,10 +218,7 @@ namespace Serverless.Forum.Pages
 
         private string CalculateMD5Hash(string input)
         {
-            var md5 = MD5.Create();
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            byte[] hash = md5.ComputeHash(inputBytes);
-
+            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(input));
             var sb = new StringBuilder();
             for (int i = 0; i < hash.Length; i++)
             {
