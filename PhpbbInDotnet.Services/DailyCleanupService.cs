a31430f8b5f0f7c58ea2d3f3849cb8d1eb64b372
Costin Banu <costinbanu87@gmail.com>
2021-10-04 22:43:46 +0200

    fixed bugs (unread posts shown as read; cleanup logic; cascade post delete)

--- PhpbbInDotnet.Services/DailyCleanupService.cs
+++ PhpbbInDotnet.Services/DailyCleanupService.cs
@@ -8,6 +8,8 @@ using PhpbbInDotnet.Objects;
 using PhpbbInDotnet.Utilities;
 using Serilog;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -36,19 +38,25 @@ namespace PhpbbInDotnet.Services
             var logger = scope.ServiceProvider.GetService<ILogger>();
             var writingToolsService = scope.ServiceProvider.GetService<WritingToolsService>();
 
-            await Task.WhenAll(
-                CleanRecycleBin(config, dbContext, utils, storageService, logger, stoppingToken),
-                ResyncOrphanFiles(config, dbContext, stoppingToken),
-                ResyncForumsAndTopics(dbContext, stoppingToken)
-            );
-
-            //either stop now, before doing permanent deletions, either not at all
-            stoppingToken.ThrowIfCancellationRequested();
-
-            var (Message, IsSuccess) = await writingToolsService.DeleteOrphanedFiles();
-            if (!(IsSuccess ?? false) && !string.IsNullOrWhiteSpace(Message))
+            try
             {
-                logger.Warning(Message);
+                await Task.WhenAll(
+                    CleanRecycleBin(config, dbContext, utils, storageService, logger, stoppingToken),
+                    ResyncOrphanFiles(config, dbContext, stoppingToken),
+                    ResyncForumsAndTopics(dbContext, stoppingToken)
+                );
+
+                stoppingToken.ThrowIfCancellationRequested();
+
+                var (Message, IsSuccess) = await writingToolsService.DeleteOrphanedFiles();
+                if (!(IsSuccess ?? false) && !string.IsNullOrWhiteSpace(Message))
+                {
+                    logger.Warning(Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Failed at least one daily cleanup task. Application will continue.");
             }
         }
 
@@ -76,7 +84,6 @@ namespace PhpbbInDotnet.Services
                 select utils.DecompressObject<PostDto>(i.Content)
             );
 
-            //either stop now, before doing permanent deletions, either not at all
             stoppingToken.ThrowIfCancellationRequested();
 
             var deleteResults = from p in posts
@@ -112,7 +119,6 @@ namespace PhpbbInDotnet.Services
 
             mismatchedOrphans.ForEach(a => a.IsOrphan = 1);
 
-            //either stop now, before doing permanent updates, either not at all
             stoppingToken.ThrowIfCancellationRequested();
 
             await dbContext.SaveChangesAsync(CancellationToken.None);
@@ -122,51 +128,87 @@ namespace PhpbbInDotnet.Services
         {
             var conn = await dbContext.GetDbConnectionAsync();
 
-            var mismatchLastPostForumsTask = conn.QueryAsync<PhpbbForums, PhpbbPosts, (PhpbbForums Forum, PhpbbPosts Post)>(
-                sql: @"WITH last_posts AS (
-	                    SELECT *
-	                    FROM phpbb_posts
-	                    GROUP BY forum_id
-	                    ORDER BY post_time DESC limit 1
-	                )
+            var postsWithWrongForumId = await (
+                from p in dbContext.PhpbbPosts.AsNoTracking()
+
+                join t in dbContext.PhpbbTopics.AsNoTracking()
+                on p.TopicId equals t.TopicId
+                into joinedTopics
+
+                from jt in joinedTopics
+                where p.ForumId != jt.ForumId
+                select new
+                {
+                    Post = p,
+                    jt.ForumId
+                }
+            ).ToListAsync();
+            var updatedPosts = postsWithWrongForumId.Select(x =>
+            {
+                x.Post.ForumId = x.ForumId;
+                return x.Post;
+            });
+
+            dbContext.PhpbbPosts.UpdateRange(updatedPosts);
+
+            stoppingToken.ThrowIfCancellationRequested();
+
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+
+            var forumsHavingWrongLastPostTask = conn.QueryAsync<PhpbbForums, PhpbbPosts, (PhpbbForums Forum, PhpbbPosts Post)>(
+                sql: @"WITH maxes AS (
+	                    SELECT forum_id, MAX(post_time) AS post_time
+                            FROM phpbb_posts
+                            GROUP BY forum_id
+                        ), last_posts AS (
+                        SELECT DISTINCT p.*
+                        FROM phpbb_posts p
+                        JOIN maxes m ON p.forum_id = m.forum_id AND p.post_time = m.post_time
+                    )
                     SELECT f.*, lp.*
-                    FROM phpbb_forums f
-                    JOIN last_posts lp ON f.forum_id = lp.forum_id
-                    WHERE f.forum_last_post_id <> lp.post_id;",
+                        FROM phpbb_forums f
+                        JOIN last_posts lp ON f.forum_id = lp.forum_id
+                        WHERE lp.post_id <> f.forum_last_post_id",
                 map: (forum, post) => (forum, post),
                 splitOn: "post_id"
             );
-            var mismatchLastPostTopicsTask = conn.QueryAsync<PhpbbTopics, PhpbbPosts, (PhpbbTopics Topic, PhpbbPosts Post)>(
-                sql: @"WITH last_posts AS (
-	                    SELECT *
-	                    FROM phpbb_posts
-	                    GROUP BY topic_id
-	                    ORDER BY post_time DESC limit 1
-	                    )
+            var topicsHavingWrongLastPostTask = conn.QueryAsync<PhpbbTopics, PhpbbPosts, (PhpbbTopics Topic, PhpbbPosts Post)>(
+                sql: @"WITH maxes AS (
+	                    SELECT topic_id, MAX(post_time) AS post_time
+                            FROM phpbb_posts
+                            GROUP BY topic_id
+                        ), last_posts AS (
+                        SELECT DISTINCT p.*
+                        FROM phpbb_posts p
+                        JOIN maxes m ON p.topic_id = m.topic_id AND p.post_time = m.post_time
+                    )
                     SELECT t.*, lp.*
-                    FROM phpbb_topics t
-                    JOIN last_posts lp ON t.topic_id = lp.topic_id
-                    WHERE t.topic_last_post_id <> lp.post_id;",
+                        FROM phpbb_topics t
+                        JOIN last_posts lp ON t.topic_id = lp.topic_id
+                        WHERE lp.post_id <> t.topic_last_post_id",
                 map: (topic, post) => (topic, post),
                 splitOn: "post_id"
             );
-            var mismatchFirstPostTopicsTask = conn.QueryAsync<PhpbbTopics, PhpbbPosts, (PhpbbTopics Topic, PhpbbPosts Post)>(
-                sql: @"WITH first_posts AS (
-	                    SELECT *
-	                    FROM phpbb_posts
-	                    GROUP BY topic_id
-	                    ORDER BY post_time ASC limit 1
-	                    )
+            var topicsHavingWrongFirstPostTask = conn.QueryAsync<PhpbbTopics, PhpbbPosts, (PhpbbTopics Topic, PhpbbPosts Post)>(
+                sql: @"WITH mins AS (
+	                    SELECT topic_id, MIN(post_time) AS post_time
+                            FROM phpbb_posts
+                            GROUP BY topic_id
+                        ), first_posts AS (
+                        SELECT DISTINCT p.*
+                        FROM phpbb_posts p
+                        JOIN mins m ON p.topic_id = m.topic_id AND p.post_time = m.post_time
+                    )
                     SELECT t.*, fp.*
-                    FROM phpbb_topics t
-                    JOIN first_posts fp ON t.topic_id = fp.topic_id
-                    WHERE t.topic_first_post_id <> fp.post_id;",
+                        FROM phpbb_topics t
+                        JOIN first_posts fp ON t.topic_id = fp.topic_id
+                        WHERE fp.post_id <> t.topic_first_post_id",
                 map: (topic, post) => (topic, post),
                 splitOn: "post_id"
             );
-            await Task.WhenAll(mismatchLastPostForumsTask, mismatchLastPostTopicsTask, mismatchFirstPostTopicsTask);
+            await Task.WhenAll(forumsHavingWrongLastPostTask, topicsHavingWrongLastPostTask, topicsHavingWrongFirstPostTask);
 
-            var forumsToUpdate = await Task.WhenAll((await mismatchLastPostForumsTask).Select(async item => 
+            var fixedForumsTask = Task.WhenAll((await forumsHavingWrongLastPostTask).Select(async item =>
             {
                 var user = await dbContext.PhpbbUsers.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == item.Post.PosterId);
                 item.Forum.ForumLastPostId = item.Post.PostId;
@@ -177,7 +219,7 @@ namespace PhpbbInDotnet.Services
                 item.Forum.ForumLastPosterColour = user?.UserColour ?? Constants.DEFAULT_USER_COLOR;
                 return item.Forum;
             }));
-            var topicsToUpdate = await Task.WhenAll((await mismatchLastPostTopicsTask).Select(async item =>
+            var fixedLastPostTopicsTask = Task.WhenAll((await topicsHavingWrongLastPostTask).Select(async item =>
             {
                 var user = await dbContext.PhpbbUsers.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == item.Post.PosterId);
                 item.Topic.TopicLastPostId = item.Post.PostId;
@@ -187,22 +229,36 @@ namespace PhpbbInDotnet.Services
                 item.Topic.TopicLastPosterName = user?.Username ?? Constants.ANONYMOUS_USER_NAME;
                 item.Topic.TopicLastPosterColour = user?.UserColour ?? Constants.DEFAULT_USER_COLOR;
                 return item.Topic;
-            }).Union((await mismatchFirstPostTopicsTask).Select(async item =>
+            }));
+            var fixedFirstPostTopicsTask = Task.WhenAll((await topicsHavingWrongFirstPostTask).Select(async item =>
             {
                 var user = await dbContext.PhpbbUsers.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == item.Post.PosterId);
                 item.Topic.TopicFirstPostId = item.Post.PostId;
                 item.Topic.TopicFirstPosterName = user?.Username ?? Constants.ANONYMOUS_USER_NAME;
                 item.Topic.TopicFirstPosterColour = user?.UserColour ?? Constants.DEFAULT_USER_COLOR;
                 return item.Topic;
-            })));
+            }));
+            await Task.WhenAll(fixedForumsTask, fixedLastPostTopicsTask, fixedFirstPostTopicsTask);
 
-            dbContext.PhpbbForums.UpdateRange(forumsToUpdate);
-            dbContext.PhpbbTopics.UpdateRange(topicsToUpdate);
+            var fixedForums = await fixedForumsTask;
+            var fixedLastPostTopics = await fixedLastPostTopicsTask;
+            var fixedFirstPostTopics = await fixedFirstPostTopicsTask;
 
-            //either stop now, before doing permanent updates, either not at all
+            dbContext.PhpbbForums.UpdateRange(fixedForums);
+            dbContext.PhpbbTopics.UpdateRange(fixedLastPostTopics.Union(fixedFirstPostTopics, new TopicEqualityComparer()));
+
             stoppingToken.ThrowIfCancellationRequested();
 
             await dbContext.SaveChangesAsync(CancellationToken.None);
+        }
+
+        class TopicEqualityComparer : IEqualityComparer<PhpbbTopics>
+        {
+            public bool Equals(PhpbbTopics x, PhpbbTopics y)
+                => x != null && y != null && x.TopicId == y.TopicId;
+
+            public int GetHashCode([DisallowNull] PhpbbTopics obj)
+                => obj.TopicId.GetHashCode();
         }
     }
 }
