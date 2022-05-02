2cf1e764fb838e78e4e81b9276c3efc2f044741d
Costin Banu <costinbanu87@gmail.com>
2022-05-02 21:52:28 +0200

    separated viewforum page functionality; some shortcut logic in new viewforum page

--- PhpbbInDotnet.Objects/PaginationDto.cs
+++ PhpbbInDotnet.Objects/PaginationDto.cs
@@ -1,4 +1,5 @@
 ﻿using JW;
+using System;
 using System.Collections.Specialized;
 using System.Web;
 
@@ -6,22 +7,36 @@ namespace PhpbbInDotnet.Objects
 {
     public class PaginationDto
     {
-        public NameValueCollection Link { get; }
+        private readonly string _url;
+        private readonly NameValueCollection _queryVars;
+        private readonly string _pageNumKey;
+
+
         public int Posts { get; }
         public int PostsPerPage { get; }
         public int CurrentPage { get; }
         public Pager Pager { get; }
         public bool HasPages => Pager.TotalPages > 1;
-        public string PageNumKey { get; }
 
         public PaginationDto(string link, int posts, int postsPerPage, int currentPage = 1, string pageNumKey = "PageNum")
         {
-            Link = HttpUtility.ParseQueryString(link);
+            var idx = link.IndexOf('?');
+            var query = idx >= 0 ? link[idx..] : "";
+            _url = idx >= 0 ? link[..idx] : link;
+            _queryVars = HttpUtility.ParseQueryString(query);
+            _pageNumKey = pageNumKey;
+
             Posts = posts;
             PostsPerPage = postsPerPage;
             CurrentPage = currentPage;
             Pager = new Pager(Posts, CurrentPage, PostsPerPage, 7);
-            PageNumKey = pageNumKey;
         }
+
+        public void UpdatePageNum(string value)
+        {
+            _queryVars.Set(_pageNumKey, value);
+        }
+
+        public string Url => $"{_url}?{_queryVars}";
     }
 }
