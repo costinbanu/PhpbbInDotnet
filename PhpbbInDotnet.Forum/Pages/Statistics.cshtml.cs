86da0e698c8657415fb306a7851e32a2790ec2ef
Costin Banu <costinbanu87@gmail.com>
2022-10-09 17:04:50 +0200

    made stats available only for registered users and tweaked the UI

--- PhpbbInDotnet.Forum/Pages/Statistics.cshtml.cs
+++ PhpbbInDotnet.Forum/Pages/Statistics.cshtml.cs
@@ -1,14 +1,17 @@
+using LazyCache;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using PhpbbInDotnet.Database;
 using PhpbbInDotnet.Domain;
+using PhpbbInDotnet.Languages;
 using PhpbbInDotnet.Objects;
 using PhpbbInDotnet.Services;
+using Serilog;
 using System;
 using System.Threading.Tasks;
 
 namespace PhpbbInDotnet.Forum.Pages
 {
-    public class StatisticsModel : PageModel
+    public class StatisticsModel : AuthenticatedPageModel
     {
         [BindProperty]
         public StatisticsPeriod? Period { get; set; }
@@ -17,34 +20,37 @@ namespace PhpbbInDotnet.Forum.Pages
 
         private readonly IStatisticsService _statisticsService;
 
-        public StatisticsModel(IStatisticsService statisticsService)
+        public StatisticsModel(IForumDbContext context, IForumTreeService forumService, IUserService userService, IAppCache cache, ILogger logger, 
+            ITranslationProvider translationProvider, IStatisticsService statisticsService)
+            : base(context, forumService, userService, cache, logger, translationProvider)
         {
             _statisticsService = statisticsService;
         }
 
-        public void OnGet()
-        {
+        public Task<IActionResult> OnGet()
+            => WithRegisteredUser(_ => Task.FromResult<IActionResult>(Page()));
 
-        }
-
-        public async Task OnPost()
-        {
-            if (Period is not null)
+        public Task<IActionResult> OnPost()
+            => WithRegisteredUser(async _ =>
             {
-                var now = DateTime.UtcNow;
-                DateTime? startTime = Period switch
+                if (Period is not null)
                 {
-                    StatisticsPeriod.TwentyFourHours => now.AddHours(-24),
-                    StatisticsPeriod.SevenDays => now.AddDays(-7),
-                    StatisticsPeriod.ThirtyDays => now.AddDays(-30),
-                    StatisticsPeriod.SixMonths => now.AddMonths(-6),
-                    StatisticsPeriod.OneYear => now.AddYears(-1),
-                    StatisticsPeriod.AllTime => null,
-                    _ => throw new NotImplementedException($"Unknown value '{Period}' for {nameof(Period)}.")
-                };
+                    var now = DateTime.UtcNow;
+                    DateTime? startTime = Period switch
+                    {
+                        StatisticsPeriod.TwentyFourHours => now.AddHours(-24),
+                        StatisticsPeriod.SevenDays => now.AddDays(-7),
+                        StatisticsPeriod.ThirtyDays => now.AddDays(-30),
+                        StatisticsPeriod.SixMonths => now.AddMonths(-6),
+                        StatisticsPeriod.OneYear => now.AddYears(-1),
+                        StatisticsPeriod.AllTime => null,
+                        _ => throw new NotImplementedException($"Unknown value '{Period}' for {nameof(StatisticsPeriod)}.")
+                    };
 
-                Result = await _statisticsService.GetTimedStatistics(startTime);
-            }
-        }
+                    Result = await _statisticsService.GetTimedStatistics(startTime);
+                }
+
+                return Page();
+            });
     }
 }
