7b7f8557e0e112f0429cf8234e5f4f5e736593ea
Costin Banu <costinbanu87@gmail.com>
2022-11-01 18:14:21 +0100

    fixed voting permission bug

--- PhpbbInDotnet.Forum/Pages/CustomPartials/_PollPartial.cshtml.cs
+++ PhpbbInDotnet.Forum/Pages/CustomPartials/_PollPartial.cshtml.cs
@@ -7,33 +7,43 @@ namespace PhpbbInDotnet.Forum.Pages.CustomPartials
 {
     public class _PollPartialModel : PageModel
     {
-        public AuthenticatedUserExpanded? CurrentUser { get; set; }
+        public AuthenticatedUserExpanded CurrentUser { get; }
 
-        public PollDto? Poll { get; set; }
+        public PollDto Poll { get; }
 
-        public int TopicId { get; set; }
+        public int TopicId { get; }
 
-        public int PageNum { get; set; }
+        public int PageNum { get; }
 
-        public string? QueryString { get; set; }
+        public string? QueryString { get; }
 
-        public bool IsCurrentUserMod { get; set; }
+        public bool IsCurrentUserMod { get; }
 
-        public bool IsCurrentUserAdmin { get; set; }
+        public bool IsCurrentUserAdmin { get; }
 
-        public bool IsPreview { get; set; }
+        public bool IsPreview { get; }
 
-        private bool? _canVoteNow;
+        public bool CanVoteNow { get; }
 
-        public bool CanVoteNow => _canVoteNow ??= !(
-            (CurrentUser?.UserId ?? Constants.ANONYMOUS_USER_ID) <= 1 ||
-            (Poll?.PollEnded ?? false) ||
-            (
-                (Poll?.PollOptions?.Any(o => o.PollOptionVoters?.Any(v => v.UserId == (CurrentUser?.UserId ?? Constants.ANONYMOUS_USER_ID)) ?? false) ?? false) && 
-                !(Poll?.VoteCanBeChanged ?? false)
-            )
-        );
+        public string Language => CurrentUser?.Language ?? Constants.DEFAULT_LANGUAGE;
 
-        public string Language { get; set; } = Constants.DEFAULT_LANGUAGE;
+        public _PollPartialModel(AuthenticatedUserExpanded currentUser, PollDto poll, bool isPreview, int topicId = 0, int pageNum = 0, string? queryString = null, bool isCurrentUserMod = false, bool isCurrentUserAdmin = false, bool isTopicLocked = false)
+        {
+            CurrentUser = currentUser;
+            Poll = poll;
+            TopicId = topicId;
+            PageNum = pageNum;
+            QueryString = queryString;
+            IsCurrentUserMod = isCurrentUserMod;
+            IsCurrentUserAdmin = isCurrentUserAdmin;
+            IsPreview = isPreview;
+            CanVoteNow = !(
+                isPreview ||
+                (isTopicLocked && !isCurrentUserMod) ||
+                currentUser.IsAnonymous ||
+                Poll.PollEnded  ||
+                (Poll.PollOptions?.Any(o => o.PollOptionVoters?.Any(v => v.UserId == currentUser.UserId) == true) == true && Poll.VoteCanBeChanged != true)
+            );
+        }
     }
 }
