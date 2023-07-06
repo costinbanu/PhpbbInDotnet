e132f1a93f1551e123070d471c8f5b76aebe544a
Costin Banu <costinbanu87@gmail.com>
2023-07-06 18:16:08 +0200

    optimized some stored procs

--- PhpbbInDotnet.Database.SetupApp/StoredProcedures/SqlServer/get_post_tracking.sql
+++ PhpbbInDotnet.Database.SetupApp/StoredProcedures/SqlServer/get_post_tracking.sql
@@ -1,4 +1,6 @@
-﻿/****** Object:  StoredProcedure [dbo].[get_post_tracking]    Script Date: 04.07.2023 18:14:15 ******/
+﻿USE [forum]
+GO
+/****** Object:  StoredProcedure [dbo].[get_post_tracking]    Script Date: 06.07.2023 18:06:27 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -23,13 +25,19 @@ AS
 		SELECT TOP 1 @user_lastmark = user_lastmark
 		  FROM phpbb_users
 		 WHERE user_id = @user_id;
-	
+
+		 CREATE TABLE #mark_times (topic_id int INDEX ix_topic_id CLUSTERED,
+								   forum_id int INDEX ix_forum_id,
+								   topic_last_post_time bigint,
+								   topic_mark_time bigint,
+								   forum_mark_time bigint);
+
+		INSERT INTO #mark_times
 		SELECT t.topic_id, 
 			   t.forum_id,
 			   t.topic_last_post_time,
 			   tt.mark_time as topic_mark_time, 
 			   ft.mark_time as forum_mark_time
-		  INTO #mark_times
 		  FROM phpbb_topics t
 		  LEFT JOIN phpbb_topics_track tt ON tt.topic_id = t.topic_id AND tt.user_id = @user_id
 		  LEFT JOIN phpbb_forums_track ft ON ft.forum_id=t.forum_id AND ft.user_id = @user_id
@@ -44,5 +52,5 @@ AS
 		   AND post_time > @user_lastmark
 		   AND p.post_time > coalesce(topic_mark_time, forum_mark_time, @user_lastmark, 0)
 		 GROUP BY m.forum_id, m.topic_id, m.topic_last_post_time, m.topic_mark_time, m.forum_mark_time;
-	END
+	END;
 END
--- PhpbbInDotnet.Database.SetupApp/StoredProcedures/SqlServer/search_posts.sql
+++ PhpbbInDotnet.Database.SetupApp/StoredProcedures/SqlServer/search_posts.sql
@@ -1,10 +1,10 @@
-﻿/****** Object:  StoredProcedure [dbo].[search_posts]    Script Date: 03.07.2023 21:11:35 ******/
+﻿USE [forum]
+GO
+/****** Object:  StoredProcedure [dbo].[search_posts]    Script Date: 06.07.2023 17:21:12 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE PROCEDURE [dbo].[search_posts]
 (
     @ANONYMOUS_USER_ID int = 1,
@@ -19,57 +19,14 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-	DECLARE @searchable_forums_table TABLE(forum_id int INDEX ix_forum_id CLUSTERED);
-	INSERT INTO @searchable_forums_table
+	CREATE TABLE #searchable_forums_table (forum_id int INDEX ix_forum_id CLUSTERED);
+	INSERT INTO #searchable_forums_table
 	SELECT value FROM string_split(@searchable_forums, ',');
 
-	DECLARE @filtered_posts TABLE(post_id int INDEX ix_post_id CLUSTERED);
-	INSERT INTO @filtered_posts
-	SELECT p.post_id
-	  FROM phpbb_posts p
-	  JOIN @searchable_forums_table sft ON sft.forum_id = p.forum_id
-	 WHERE (@topic_id = 0 OR @topic_id = p.topic_id)
-       AND (@author_id = 0 OR @author_id = p.poster_id);
+	CREATE TABLE #filtered_posts (post_id int INDEX ix_post_id CLUSTERED);
 
-	DECLARE @posts TABLE (forum_id int not null, 
-						  topic_id int not null, 
-						  post_id int not null INDEX ix_post_id CLUSTERED, 
-						  post_subject nvarchar(255) not null, 
-						  post_text nvarchar(max) not null, 
-						  author_id int not null, 
-						  bbcode_uid nvarchar(8) not null, 
-						  post_time bigint not null,
-						  post_edit_count int not null,
-						  post_edit_reason nvarchar(255) not null,
-						  post_edit_time bigint not null,
-						  [ip] nvarchar(40) not null,
-						  post_username nvarchar(255) not null,
-						  post_edit_user int not null,
-						  total_count int not null);
-
-	IF (coalesce(@search_text, '') = '')
-		INSERT INTO @posts
-		SELECT p.forum_id,
-			   p.topic_id,
-			   p.post_id,
-			   p.post_subject,
-			   p.post_text,
-			   p.poster_id as author_id,
-			   p.bbcode_uid,
-			   p.post_time,
-			   p.post_edit_count,
-			   p.post_edit_reason,
-			   p.post_edit_time,
-			   p.poster_ip as ip,
-			   p.post_username,
-			   p.post_edit_user,
-			   count(1) over() as total_count
-		  FROM phpbb_posts p
-		  JOIN @filtered_posts fp on fp.post_id = p.post_id
-		 ORDER BY p.post_time DESC
-		OFFSET @skip ROWS FETCH NEXT @take ROWS ONLY;
-
-	ELSE
+	SET @search_text = ltrim(rtrim(@search_text));
+	IF (coalesce(@search_text, '') <> '')
 	BEGIN
 		WHILE CHARINDEX('  ', @search_text) > 0 
 			SET @search_text = REPLACE(@search_text, '  ', ' ');
@@ -77,7 +34,29 @@ BEGIN
 		IF (@search_text NOT LIKE '"%"')
 			SET @search_text = REPLACE(@search_text, ' ', ' OR ');
 
-		INSERT INTO @posts
+		INSERT INTO #filtered_posts
+		SELECT p.post_id
+		  FROM phpbb_posts p
+		 WHERE CONTAINS(*, @search_text);
+	END;
+
+	CREATE TABLE #posts (forum_id int not null, 
+						 topic_id int not null, 
+						 post_id int UNIQUE not null, 
+						 post_subject nvarchar(255) not null, 
+						 post_text nvarchar(max) not null, 
+						 author_id int not null INDEX ix_author_id CLUSTERED, 
+						 bbcode_uid nvarchar(8) not null, 
+						 post_time bigint not null,
+						 post_edit_count int not null,
+						 post_edit_reason nvarchar(255) not null,
+						 post_edit_time bigint not null,
+						 [ip] nvarchar(40) not null,
+						 post_username nvarchar(255) not null,
+						 post_edit_user int not null,
+						 total_count int not null);
+
+		INSERT INTO #posts
 		SELECT p.forum_id,
 			   p.topic_id,
 			   p.post_id,
@@ -94,11 +73,12 @@ BEGIN
 			   p.post_edit_user,
 			   count(1) over() as total_count
 		  FROM phpbb_posts p
-		  JOIN @filtered_posts fp on fp.post_id = p.post_id
-		 WHERE CONTAINS(p.post_text, @search_text)
+		  JOIN #searchable_forums_table sft ON sft.forum_id = p.forum_id
+		  JOIN #filtered_posts fp ON fp.post_id = p.post_id
+		 WHERE (@topic_id = 0 OR @topic_id = p.topic_id)
+		   AND (@author_id = 0 OR @author_id = p.poster_id)
 		 ORDER BY p.post_time DESC
 		OFFSET @skip ROWS FETCH NEXT @take ROWS ONLY;
-	END
 
 	SELECT DISTINCT u.user_id, 
 			COALESCE(r1.rank_id, r2.rank_id) AS rank_id, 
@@ -106,7 +86,7 @@ BEGIN
 	   INTO #ranks
 	   FROM phpbb_users u
 	   JOIN phpbb_groups g ON u.group_id = g.group_id
-	   JOIN @posts p on p.author_id = u.user_id
+	   JOIN #posts p on p.author_id = u.user_id
 	   LEFT JOIN phpbb_ranks r1 ON u.user_rank = r1.rank_id
 	   LEFT JOIN phpbb_ranks r2 ON g.group_rank = r2.rank_id
 
@@ -116,12 +96,9 @@ BEGIN
 		   a.user_avatar as author_avatar,
 		   e.username as post_edit_user,
 		   r.rank_title
-	  FROM @posts p
+	  FROM #posts p
 	  LEFT JOIN #ranks r ON p.author_id = r.user_id
 	  LEFT JOIN phpbb_users a ON p.author_id = a.user_id
-	  LEFT JOIN phpbb_users e ON p.post_edit_user = e.user_id
-	 ORDER BY p.post_time DESC;
+	  LEFT JOIN phpbb_users e ON p.post_edit_user = e.user_id;
 
 END
-GO
-
