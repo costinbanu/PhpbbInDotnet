866764b86a3fce1ec1a7e69b63b0541add98a393
Costin Banu <costinbanu87@gmail.com>
2023-07-18 11:51:27 +0200

    SP optimizations; fixed error on counting anonymous sessions
    (cherry picked from commit ab3855849ee2277f0f1a6d12a3ba1e9f36bbc49f)

--- PhpbbInDotnet.Database.SetupApp/StoredProcedures/SqlServer/get_new_posts.sql
+++ PhpbbInDotnet.Database.SetupApp/StoredProcedures/SqlServer/get_new_posts.sql
@@ -11,12 +11,12 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @allowed_topics TABLE(topic_id int INDEX ix_forum_id CLUSTERED);
-	INSERT INTO @allowed_topics
+	CREATE TABLE #allowed_topics(topic_id int INDEX ix_forum_id CLUSTERED);
+	INSERT INTO #allowed_topics
 	SELECT value FROM string_split(@topic_list, ',');	
 
-	DECLARE @restricted_forums TABLE(forum_id int INDEX ix_forum_id CLUSTERED);
-	INSERT INTO @restricted_forums
+	CREATE TABLE #restricted_forums(forum_id int INDEX ix_forum_id CLUSTERED);
+	INSERT INTO #restricted_forums
 	SELECT value FROM string_split(@restricted_forum_list, ',');	
 
     SELECT t.topic_id, 
@@ -31,8 +31,8 @@ BEGIN
 		   t.topic_last_post_id
 	  FROM phpbb_topics t
 	  JOIN phpbb_posts p ON t.topic_id = p.topic_id
-	  JOIN @allowed_topics [at] ON [at].topic_id = t.topic_id
-	  LEFT JOIN @restricted_forums rf ON rf.forum_id = t.forum_id
+	  JOIN #allowed_topics [at] ON [at].topic_id = t.topic_id
+	  LEFT JOIN #restricted_forums rf ON rf.forum_id = t.forum_id
 	 WHERE rf.forum_id IS NULL
 	 GROUP BY t.topic_id, t.topic_title, t.forum_id, t.topic_views, t.topic_last_poster_id, t.topic_last_poster_name, t.topic_last_post_time, t.topic_last_poster_colour, t.topic_last_post_id
 	 ORDER BY t.topic_last_post_time DESC
