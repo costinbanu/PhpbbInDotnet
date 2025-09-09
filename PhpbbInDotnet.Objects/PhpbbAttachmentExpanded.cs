ab71c50299fbe10e730ff27212be19747a6afbb1
Costin Banu <costinbanu87@gmail.com>
2025-09-09 17:31:52 +0200

    add order column to table and entities

--- PhpbbInDotnet.Objects/PhpbbAttachmentExpanded.cs
+++ PhpbbInDotnet.Objects/PhpbbAttachmentExpanded.cs
@@ -22,6 +22,7 @@ namespace PhpbbInDotnet.Objects
             Filetime = @base.Filetime;
             Thumbnail = @base.Thumbnail;
             DraftId = @base.DraftId;
+            Order = @base.Order;
             ForumId = forumId;
         }
 
